f250339d8649e0ee9b309e382a36b550e7cbfa24
Jardel Barbosa <86034190+Jardel894@users.noreply.github.com>
2024-01-17 23:46:39 -0300

    Projeto Criado

---
+++ src/main/java/br/com/vollclinica/VollClinicaApplication.java
@@ -0,0 +1,13 @@
+package br.com.vollclinica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VollClinicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VollClinicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/vollclinica/VollClinicaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vollclinica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VollClinicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
